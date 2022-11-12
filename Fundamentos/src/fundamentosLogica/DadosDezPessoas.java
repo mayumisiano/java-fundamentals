e235d971d4c4b1a686e21b785884afa7532e9c48
Paula Mayumi <mayumi_siano10@hotmail.com>
2022-11-12 14:33:51 -0300

    Atividades de Lógica em Java - Lista 1

---
+++ Fundamentos/src/fundamentosLogica/DadosDezPessoas.java
@@ -0,0 +1,44 @@
+package fundamentosLogica;
+
+import java.util.Scanner;
+
+public class DadosDezPessoas {
+
+	public static void main(String[] args) {
+		float altura, maiorAltura=0, menorAltura=0, mediaAltura = 0, somaAltura = 0,
+			  numeroMulheres=0,numeroHomens=0;
+		char genero;		
+		
+		Scanner sc = new Scanner(System.in);				
+		
+		for( int x=0; x<=2; x++) {
+			
+			System.out.println("Insira seu gênero (M/F): ");
+			genero = sc.next().charAt(0);
+			System.out.println("Insira sua altura: ");
+			altura = sc.nextFloat();
+			
+			if(altura > maiorAltura) {
+				maiorAltura = altura;
+			}else if(altura > menorAltura) {
+				menorAltura = altura;
+			}			
+			
+			if(genero == 'M') {
+				somaAltura += altura;
+				numeroHomens++;
+			}else if(genero == 'F') {
+				numeroMulheres++;
+			}else {
+				System.out.println("Opção Inválida, digite M/F");
+			}
+			
+			mediaAltura = somaAltura/numeroHomens;				
+						
+		}
+		System.out.println("\nA maior altura foi: "+maiorAltura);
+		System.out.println("\nA menor altura foi: "+menorAltura);
+		System.out.println("\nO número de mulheres é: "+numeroMulheres);
+		System.out.printf("\nA média de altura dos homens é: %.2f",mediaAltura);		
+	}
+}
---
+++ Fundamentos/src/fundamentosLogica/QuantidadeCavalos.java
@@ -0,0 +1,24 @@
+package fundamentosLogica;
+
+import java.util.Scanner;
+
+public class QuantidadeCavalos {
+
+	public static void main(String[] args) {
+		float m, h, t;
+		double cavalos;	
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Insira a massa em kg: ");
+		m = sc.nextFloat();
+		System.out.println("Insira a altura em m: ");
+		h = sc.nextFloat();
+		System.out.println("Insira o tempo em s: ");
+		t = sc.nextFloat();
+		
+		cavalos = ((m*h) / t) / 745.6999;
+		
+		System.out.printf("A quantidade de cavalos deverá ser: %.2f",cavalos);
+	}
+
+}
